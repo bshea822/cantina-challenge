5c5a9f846a19fab036ebf20ab710cd3a79e27ed9
Brendon Shea <brendonshea@Brendons-MacBook-Air.local>
2018-11-19 17:55:58 -0500

    Refactored further, added validations to user input

--- script.rb
+++ script.rb
@@ -4,13 +4,11 @@ require 'json'
 json_file = File.read('./SystemViewController.json')
 json = JSON.parse(json_file)
 
-puts "Welcome!\n\nTo begin, please enter a selector name to see the views associated with the given selector.  To exit the program, simply enter 'exit'.\n\n"
-
 @classes = {}
 @classNames = {}
 @identifiers = {}
 
-def parse_json(data)
+def self.parse_json(data)
   data.each do |key, value|
     if key === 'class'
       if @classes[value]
@@ -32,6 +30,8 @@ def parse_json(data)
       else
         @identifiers[value] = [data]
       end
+    elsif key === 'control'
+      parse_json(value)
     elsif key === 'contentView' || key === 'subviews'
       if key === 'contentView'
         value['subviews'].each do |new_data|
@@ -48,13 +48,47 @@ end
 
 parse_json(json)
 
-input = ""
-binding.pry
+puts "Welcome!\n\nYou can use this program by entering in selectors to find the corresponding views!\n\nInstructions:\nEnter a selector name with a prefix symbol to pull up the corresponding views.\n\nPrefixes:\n@ = class\n. = className\n# = identifier\n\nExamples:\n@StackView - Shows all views with a class of 'StackView'\n.container - Shows all views with a CSS className of 'container'\n#videoMode - Shows all views with an identifer of 'videoMode'\n\nEnter 'exit' if you'd like to exit the program.\n\n"
 
-# while input != 'exit' do
-#   print "Enter Selector: "
-#   input = gets.chomp
-#
-#
-#
-# end
+input = ""
+
+while input != 'exit' do
+  print "Enter Selector: "
+  input = gets.chomp
+  if input[0] === '@' || input[0] === '.' || input[0] === '#'
+    if input[0] === '@'
+      if @classes[input.delete '@'] != nil
+        @classes[input.delete '@'].each do |output|
+          puts output
+          puts "\n\n"
+        end
+      else
+        puts "That selector is not found, please check your spelling and try again."
+      end
+    end
+
+    if input[0] === '.'
+      if @classes[input.delete '.'] != nil
+        @classNames[input.delete '.'].each do |output|
+          puts output
+          puts "\n\n"
+        end
+      else
+        puts "That selector is not found, please check your spelling and try again."
+      end
+    end
+
+    if input[0] === '#'
+      if @classes[input.delete '#'] != nil
+        @identifiers[input.delete '#'].each do |output|
+          puts output
+          puts "\n\n"
+        end
+      else
+        puts "That selector is not found, please check your spelling and try again."
+      end
+    end
+  else
+    puts "You did not enter a supported prefix.  Supported prefixes are '@', '.', and '#'.  Please use one of these prefixes in conjunction with a selector name."
+  end
+end
